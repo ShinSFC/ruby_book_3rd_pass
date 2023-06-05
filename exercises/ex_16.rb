7d9ec9639d9c5094026b1cec802e3d721373bc96
ShinpeiH <dctechsf@gmail.com>
2023-06-04 23:23:43 -0700

    worked more on ex_16

--- exercises/ex_16.rb
+++ exercises/ex_16.rb
@@ -2,17 +2,15 @@ contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
 ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
 
-
-
 joe = contact_data[0]
 sally = contact_data[1]
 
-contacts["Joe Smith"] = [:email, :address, :phone].map! do |symbol|
-  {symbol => joe.shift}
+contacts["Joe Smith"] = [:email, :address, :phone].map! do |key|
+  {key => joe.shift}
 end
 
-contacts["Sally Johnson"] = [:email, :address, :phone].map! do |symbol|
-  {symbol => sally.shift}
+contacts["Sally Johnson"] = [:email, :address, :phone].map! do |key|
+  {key => sally.shift}
 end
 
 p contacts
